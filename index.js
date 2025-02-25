447c6162775d70403fdac8c8e31961b979321e13
alauddin <alauddinkmj@gmail.com>
2025-02-25 18:11:18 +0600

    formatted

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const verifyToken = (req, res, next) => {
 };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zn6isea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//line wrapping
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
